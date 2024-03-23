87bf594a8a23a22ff52a75c22e60e58d5718d708
Maks Solovev <msol86@mail.ru>
2024-03-23 22:12:33 +0500

    part1

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../blocks/page/page.scss';
+import '../blocks/header/header.scss';
+import '../blocks/header-menu/header-menu.scss';
+import '../blocks/sub-header/sub-header.scss';
+import '../blocks/sub-menu/sub-menu.scss';
+import '../blocks/menu/menu.scss';
+import '../blocks/about-company/about-company.scss';
---
+++ webpack.config.js
@@ -0,0 +1,123 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { SplitChunksPlugin } = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCSSWebpackPlugin = require('css-minimizer-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+console.log('isDev: ', isDev);
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+        chunks: 'all',
+    }
+    }
+
+    if (!isDev) {
+        config.minimizer = [
+            new OptimizeCSSWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config;
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+const cssLoaders = additons => {
+    const loaders = [MiniCssExtractPlugin.loader, 
+        'css-loader', 'postcss-loader'];
+        
+    if (additons) {
+        loaders.push(additons);
+    }
+
+    return loaders;
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: ['@babel/polyfill', './js/index.js'],
+    },
+    output: {
+        clean: true,
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 4200,
+        hot: isDev,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: !isDev,
+            }
+        }),
+        //new CleanWebpackPlugin() 
+        new MiniCssExtractPlugin({
+            filename: filename('css'),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                //use: ['file-loader'],
+                type: 'asset/resource',
+                
+            },
+            {
+                test: /\.css$/,
+                use: cssLoaders(),
+            },
+           /* {
+                loader: 'postcss-loader',
+                options: {
+                    plugins: [
+                        autoprefixer({
+                            browsers:['ie >= 8', 'last 4 version']
+                        })
+                    ],
+                    sourceMap: true
+                }
+            },*/
+            {
+                test: /\.less$/,
+                use: cssLoaders('less-loader'), 
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader'),
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/,
+                type: 'asset/resource',
+                //use: ['file-loader']
+            },
+            
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            }
+        ]
+    }
+};
